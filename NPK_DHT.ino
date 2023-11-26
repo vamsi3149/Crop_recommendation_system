c9b98e0e1f46734775298d7805c2b00b0087eed0
vamsi3149 <147086600+vamsi3149@users.noreply.github.com>
2023-11-26 20:07:34 +0530

    Add files via upload

---
+++ NPK_DHT.ino
@@ -0,0 +1,4 @@
+Farming faces problems due to climate change, like unpredictable weather and shifting growing seasons.
+An advanced machine learning model is used to study soil history and local methods, recommending the       right crops.
+This system considers things like temperature and humidity to give farmers useful info, dealing with climate change issues such as rainfall, temperature, and pests.
+The technology helps make farming tougher and eco-friendly, leading to more food, less harm to the environment, and better lives for farmers facing climate issues.
